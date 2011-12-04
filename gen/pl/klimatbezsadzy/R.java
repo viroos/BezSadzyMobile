0994a15b14fb40bb2376ce60fba080d9a3bb9084
Maciej Sawicki <maciej.sawicki@polidea.pl>
2011-12-04 14:26:13 +0100

    clenup - removed old, unused source

--- gen/pl/klimatbezsadzy/R.java
+++ gen/pl/klimatbezsadzy/R.java
@@ -24,9 +24,8 @@ public final class R {
         public static final int kbs_logo48=0x7f02000a;
         public static final int kbs_logo72=0x7f02000b;
         public static final int notbad=0x7f02000c;
-        public static final int notification_icon=0x7f02000d;
-        public static final int verybad=0x7f02000e;
-        public static final int widget_frame=0x7f02000f;
+        public static final int verybad=0x7f02000d;
+        public static final int widget_frame=0x7f02000e;
     }
     public static final class id {
         public static final int background=0x7f060005;
@@ -44,8 +43,7 @@ public final class R {
     }
     public static final class layout {
         public static final int info=0x7f030000;
-        public static final int invisible=0x7f030001;
-        public static final int widget=0x7f030002;
+        public static final int widget=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
